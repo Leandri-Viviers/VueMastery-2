f364d2234c01f05d19a8f6b2b89928adf6ab73ed
Leandri <lviviers@retrorabbit.co.za>
2020-08-09 00:38:32 +0200

    Lesson 10: Forms and v-model

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 Vue.component('product', {
   template: `
-    <div class="flex-column flex-1">
-      <div class="flex-1 flex-row">
+    <div class="content">
+      <div class="product">
         <div class="flex-1 flex-row align-center justify-center">
           <img :src="image"/>
         </div>
         <div class="flex-1 flex-column align-start justify-center">
           <div class="product-info">
             <!-- Sale -->
-            <div class="text-right">
+            <div class="sale">
               <p v-show="onSale" class="flex-0 badge"> On sale </p>
             </div>
             <!-- Heading -->
@@ -46,6 +46,23 @@ Vue.component('product', {
           </div>
         </div>
       </div>
+      <div class="review">
+        <div class="flex-1 flex-column align-center">
+          <div class="product-reviews">
+            <h4> Reviews </h4>
+            <p v-show="reviews.length == 0"> There are no reviews yet. </p>
+            <div v-for="review in reviews">
+              <div class="flex-row align-center justify-space-between review-text">
+                <p> {{review.name}} </p>
+                <p> {{review.rating}} &#128970; </p>
+              </div>
+              <p class="caption">{{review.review}}</p>
+              <p class="divider"></p>
+            </div>
+          </div>
+        </div>
+        <product-review @review-submitted="addReview"></product-review>
+      </div>
     </div>
   `,
   props: {
@@ -66,6 +83,7 @@ Vue.component('product', {
         'Platform height: 1 3/4"'
       ],
       sizes: [5, 6, 7, 8, 9, 10, 11, 12],
+      selectedSize: 5,
       selectedVariant: 0,
       variants: [
         {
@@ -81,14 +99,21 @@ Vue.component('product', {
           quantity: 0,
         }
       ],
+      reviews: [
+        {name:'Sara', rating:5, review:'I love the height it gives me and how stylish and edgy they are.'},
+        {name:'Rachel', rating:4, review:'Love the look and style.'},
+      ],
     }
   },
   methods: {
-    addToCart: function(){
+    addToCart(){
       this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
     },
-    updateProduct: function(index){
+    updateProduct(index){
       this.selectedVariant = index
+    },
+    addReview(productReview){
+      this.reviews.push(productReview)
     }
   },
   computed: {
@@ -121,6 +146,86 @@ Vue.component('product-details', {
       required: true
     }
   },
+})
+
+Vue.component('product-review', {
+  template: `
+      <div class="flex-1">
+        <h4> Write your review </h4>
+        <form @submit.prevent="onSubmit">
+          <div class="flex-row">
+            <div class="flex-1 flex-column">
+              <input id="name" v-model="name" class="order-1" :class="{'input-error':errors.name}"/>
+              <label for="name">Full name:</label>
+              <span class="error-message order-2"> {{errors.name}} </span>
+            </div>
+            <div class="flex-column ml-1">
+              <select id="rating" v-model.number="rating" class="order-1" :class="{'input-error':errors.name}">
+                <option v-for="rate in ratings">{{rate}}</option>
+              </select>
+              <label for="Rating">Rating:</label>
+              <span class="error-message order-2"> {{errors.rating}} </span>
+            </div>
+          </div>
+          <div class="flex-column mt-1">
+            <textarea id="review" v-model="review" rows="4" class="order-1" :class="{'input-error':errors.name}"/>
+            <label for="review">Review:</label>
+              <span class="error-message order-2"> {{errors.review}} </span>
+          </div>
+          <div>
+            <input type="submit" value="Submit review"/>
+          </div>
+        </form>
+      </div>
+    </div>
+  `,
+  data(){
+    return {
+      name: null,
+      rating: null,
+      review: null,
+      ratings: [5, 4, 3, 2, 1],
+      errors: {
+        name: null,
+        rating: null,
+        review: null,
+      },
+    }
+  },
+  methods: {
+    onSubmit(){
+      this.clearErrors()
+      if(this.name && this.review && this.rating){
+        let productReview = {
+          name: this.name,
+          rating: this.rating,
+          review: this.review
+        }
+        this.clearForm()
+        this.$emit('review-submitted', productReview)
+        return
+      }
+      this.validate()
+    },
+    clearErrors(){
+      this.errors = {
+        name: null,
+        rating: null,
+        review: null,
+      }
+    },
+    clearForm(){
+      this.name = null
+      this.rating = null
+      this.review = null
+    },
+    validate(){
+      // validation errors
+      if(!this.name) this.errors.name = 'Required'
+      if(!this.rating) this.errors.rating = 'Required'
+      if(!this.review) this.errors.review = 'Required'
+    }
+  }
 })
 
 var app = new Vue({
